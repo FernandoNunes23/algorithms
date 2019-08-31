6d3994f4a605f026e49f57b17ab5656d3f084a4a
Fernando Nunes <fernando.nunes@kinghost.com.br>
2019-08-31 20:03:05 -0300

    Inserted necessary methos for the Heap Sort

--- MaxHeap.php
+++ MaxHeap.php
@@ -3,5 +3,98 @@
 
 class MaxHeap
 {
+    private $heap;
+    private $size;
 
+    public function __construct(array $inputArray)
+    {
+        $this->heap = $inputArray;
+        $this->size = count($this->heap);
+    }
+
+    public function isEmpty()
+    {
+        return $this->size == 0;
+    }
+
+    public function peek()
+    {
+        if ($this->size == 0) {
+            return false;
+        }
+
+        return $this->heap[0];
+    }
+
+    public function remove()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    private function heapifyDown()
+    {
+
+    }
+
+    private function heapifyUp()
+    {
+
+    }
+
+    private function swap(int $indexOne, int $indexTwo)
+    {
+        $temp = $this->heap[$indexOne];
+        $this->heap[$indexOne] = $this->heap[$indexTwo];
+        $this->heap[$indexTwo] = $temp;
+    }
+
+    private function getLeftChildIndex(int $parentIndex)
+    {
+        return 2 * $parentIndex + 1;
+    }
+
+    private function getRightChildIndex(int $parentIndex)
+    {
+        return 2 * $parentIndex + 2;
+    }
+
+    private function getParentIndex(int $childIndex)
+    {
+        return ($childIndex - 1) / 2;
+    }
+
+    private function hasLeftChild(int $index)
+    {
+        return $this->getLeftChildIndex($index) < $this->size;
+    }
+
+    private function hasRightChild(int $index)
+    {
+        return $this->getRightChildIndex($index) < $this->size;
+    }
+
+    private function hasParent(int $index)
+    {
+        return $this->getParentIndex($index) >= 0;
+    }
+
+    private function getLeftChild (int $index)
+    {
+        return $this->heap[$this->getLeftChildIndex($index)];
+    }
+
+    private function getRightChild (int $index)
+    {
+        return $this->heap[$this->getRightChildIndex($index)];
+    }
+
+    private function parent (int $index)
+    {
+        return $this->heap[$this->getParentIndex($index)];
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
-var_dump('aaa');exit;
+$array = [20,80,32,12,1,3,45,12,31,3];
+
+$maxHeap = new MaxHeap($array);
