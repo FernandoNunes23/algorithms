358b639303d8267f579b2d6a7b214efdbb452cd6
Fernando Nunes <fernando.nunes@kinghost.com.br>
2019-09-02 00:12:02 -0300

    Added QuickSort algorithm and somes execution times tests

---
+++ QuickSort.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class QuickSort
+{
+    public function sort($array)
+    {
+        $loe = $gt = array();
+        if(count($array) < 2)
+        {
+            return $array;
+        }
+        $pivot_key = key($array);
+        $pivot = array_shift($array);
+        foreach($array as $val)
+        {
+            if($val <= $pivot)
+            {
+                $loe[] = $val;
+            }elseif ($val > $pivot)
+            {
+                $gt[] = $val;
+            }
+        }
+
+        return array_merge($this->sort($loe),array($pivot_key=>$pivot),$this->sort($gt));
+    }
+}
--- index.php
+++ index.php
@@ -1,19 +1,58 @@
 <?php
 
 require_once("HeapSort.php");
+require_once("QuickSort.php");
 require_once("BinarySearch.php");
 
-$array = [20,80,32,12,1,35,45,121,3,12,121,80];
+$array = [];
 
-$heapSort = new HeapSort($array);
+$time_start = microtime(true);
 
-$orderedArray = $heapSort->heapSort();
-
-foreach ($orderedArray as $k => $a) {
-    print("Value: " . $a . " ||  ");
+for ($i = 0; $i <= 100000; $i++) {
+    $array[$i] = rand(-100000, 100000);
 }
 
+$time_end = microtime(true);
+//dividing with 60 will give the execution time in minutes otherwise seconds
+$execution_time = ($time_end - $time_start);
+//execution time of the script
+echo '<b>Total Execution Time Populating Array:</b> <br>'.number_format((float) $execution_time, 10).' secs';
+
+$quickSort = new QuickSort();
+
+$time_start = microtime(true);
+
+$orderedArray = $quickSort->sort($array);
+
+$time_end = microtime(true);
+//dividing with 60 will give the execution time in minutes otherwise seconds
+$execution_time = ($time_end - $time_start);
+//execution time of the script
+echo '<br><b>Total Execution Time Quick Sorting:</b> <br>'.number_format((float) $execution_time, 10).' secs';
+
+$sort = new HeapSort($array);
+
+$time_start = microtime(true);
+
+$orderedArray = $sort->heapSort();
+
+$time_end = microtime(true);
+//dividing with 60 will give the execution time in minutes otherwise seconds
+$execution_time = ($time_end - $time_start);
+//execution time of the script
+echo '<br><b>Total Execution Time Heap Sorting:</b> <br>'.number_format((float) $execution_time, 10).' secs';
+
 $binarySearch = new BinarySearch($orderedArray);
-$result = $binarySearch->search(0, (count($orderedArray) - 1), 80);
+
+$time_start = microtime(true);
+
+$result = $binarySearch->search(0, (count($orderedArray) - 1), 3);
+
+$time_end = microtime(true);
+//dividing with 60 will give the execution time in minutes otherwise seconds
+$execution_time = ($time_end - $time_start);
+//execution time of the script
+echo '<br><b>Total Execution Time Binary Search:</b> <br>'.number_format((float) $execution_time, 10).' secs <br>';
+
 
 print('Result: ' . $result);
