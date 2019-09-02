6bd5c60b62fe6cf16e697059c903dc77b8458a16
Fernando Nunes <fernando.nunes@kinghost.com.br>
2019-09-01 23:22:09 -0300

    Finished Binary Search and Binary Search test

--- BinarySearch.php
+++ BinarySearch.php
@@ -3,5 +3,37 @@
 
 class BinarySearch
 {
+    private $array;
 
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+
+    public function search($offset, $limit, $searchParam)
+    {
+        if ($limit >= $offset)
+        {
+            $mid = ceil($offset + ($limit - $offset) / 2);
+
+            // If the element is present
+            // at the middle itself
+            if ($this->array[$mid] == $searchParam)
+                return floor($mid);
+
+            // If element is smaller than
+            // mid, then it can only be
+            // present in left subarray
+            if ($this->array[$mid] > $searchParam)
+                return $this->search($offset, $mid - 1, $searchParam);
+
+            // Else the element can only
+            // be present in right subarray
+            return $this->search($mid + 1, $limit, $searchParam);
+        }
+
+        // We reach here when element
+        // is not present in array
+        return -1;
+    }
 }
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
 require_once("HeapSort.php");
+require_once("BinarySearch.php");
 
 $array = [20,80,32,12,1,35,45,121,3,12,121,80];
 
 $heapSort = new HeapSort($array);
 
-foreach ($heapSort->heapSort() as $k => $a) {
+$orderedArray = $heapSort->heapSort();
+
+foreach ($orderedArray as $k => $a) {
     print("Value: " . $a . " ||  ");
 }
+
+$binarySearch = new BinarySearch($orderedArray);
+$result = $binarySearch->search(0, (count($orderedArray) - 1), 80);
+
+print('Result: ' . $result);
